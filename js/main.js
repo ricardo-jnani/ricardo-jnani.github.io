089627f68c5c271c52f25ba81dcd535f87c380d8
ricardo-jnani <rjg.jnani@gmail.com>
2017-08-16 12:54:53 -1000

    Setting up Github pages

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/////////////////////////
+// INTERACTION IDEAS
+/////////////////////////
+
+
+// SCROLL TO STCIKY NAV BAR
+  //When the user scrolls down past ~ 500px
+    //header becomes smaller
+    //navbar stops, becomes fixed at top of page
+
+// SHOW NAVIGATION ICONS / BUTTONS
+  //When the user scrolls down past ~ 1000px
+    //Show navigation Icons (Up & Down Arrows) fixed on right side of page
+    //If current view style is Default (Slide View)
+      //Show Index View Icon
+      //Else show Slide View Icon
+
+// ARROW NEXT & PREVIOUS SCROLL FOR THE SLIDES
+  //When the user clicks Down Arrow
+    //If currentImageNumber is < numberImages
+      //Increase currentImageNumber by 1
+    //Else make currentImageNumber = 0
+  //When the user clicks Up Arrow
+    //If currentImageNumber is > numberImages
+      //Decrease currentImageNumber by 1
+    //Else make currentImageNumber = numberImages
+
+// INDEX VIEW ICON
+  //When the user clicks on Index View Icon
+    //Add Class index-view-style (changing flexbox to have flex-basis=22% & space-between) to all slides
+    //Hide Index View Icon
+    //Show Slide View Icon
+
+// SLIDE VIEW ICON
+  //When the user clicks on Slide View Icon
+    //Add Class slide-view-style (changing flexbox to have flex-basis=22% & space-between) to all slides
+    //Hide Slide View Icon
+    //Show Index View Icon
+
+//SMOOTH SCROLL
+
+
+//FILTER CONTENT
